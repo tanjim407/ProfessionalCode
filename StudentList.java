c79ee4b75a7d295837a4490cbf078a430984924f
taifur <rahmantanjim12@gmail.com>
2023-08-22 22:18:33 +0600

    UpdatereadWriteFile

--- StudentList.java
+++ StudentList.java
@@ -3,16 +3,22 @@ import java.text.*;
 import java.util.*;
 
 public class StudentList {
+	
+	public static String getLineFromFile() throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt")));
+        String line = bufferedReader.readLine();
+        return line;
+    }
+	public static BufferedWriter getFileBufferedWriter() throws Exception {
+        return new BufferedWriter(new FileWriter("students.txt", true));
+    }
 	public static void main(String[] args) {
 
 		// Check arguments
 		if (args[0].equals("a")) {
 			System.out.println("Loading data ...");
 			try {
-				BufferedReader bufferedReader = new BufferedReader(
-						new InputStreamReader(
-								new FileInputStream("students.txt")));
-				String studentNames = bufferedReader.readLine();
+				String studentNames = getLineFromFile();
 				String names[] = studentNames.split(",");
 				for (String name : names) {
 					System.out.println(name);
@@ -24,10 +30,7 @@ public class StudentList {
 	     else if (args[0].equals("r")) {
 			System.out.println("Loading data ...");
 			try {
-				BufferedReader bufferedReader = new BufferedReader(
-						new InputStreamReader(
-								new FileInputStream("students.txt")));
-				String studentNames = bufferedReader.readLine();
+				String studentNames = getLineFromFile();
 				System.out.println(studentNames);
 				String names[] = studentNames.split(",");
 				Random random = new Random();
@@ -40,8 +43,7 @@ public class StudentList {
 		 else if (args[0].contains("+")) {
 			System.out.println("Loading data ...");
 			try {
-				BufferedWriter bufferedWriter = new BufferedWriter(
-						new FileWriter("students.txt", true));
+				BufferedWriter bufferedWriter = getFileBufferedWriter();
 				String name = args[0].substring(1);
 				Date date = new Date();
 				String dateForm = "dd/mm/yyyy-hh:mm:ss a";
@@ -57,10 +59,8 @@ public class StudentList {
 		 else if (args[0].contains("?")) {
 			System.out.println("Loading data ...");
 			try {
-				BufferedReader bufferedReader = new BufferedReader(
-						new InputStreamReader(
-								new FileInputStream("students.txt")));
-				String studentName = bufferedReader.readLine();
+
+				String studentName = getLineFromFile();
 				String names[] = studentName.split(",");
 				boolean done = false;
 				String name = args[0].substring(1);
@@ -77,10 +77,8 @@ public class StudentList {
 		else if (args[0].contains("c")) {
 			System.out.println("Loading data ...");
 			try {
-				BufferedReader bufferedReader= new BufferedReader(
-						new InputStreamReader(
-								new FileInputStream("students.txt")));
-				String studentNames = bufferedReader.readLine();
+				
+				String studentNames = getLineFromFile();
 				char names[] = studentNames.toCharArray();
 				boolean in_word = false;
 				int count = 0;
